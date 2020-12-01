b468df240a86a572513c26d5fa36824bda6a4bb2
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-01 19:34:03 +0330

    second search input and autocomplete added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     return `
-        <img src="${imgSrc}" />
-        ${movie.Title} (${movie.Year})
-      `;
+            <img src="${imgSrc}" />
+            ${movie.Title} (${movie.Year})
+          `;
   },
 
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
-  },
   inputValue(movie) {
     return movie.Title;
   },
@@ -26,9 +22,27 @@ createAutoComplete({
 
     return response.data.Search;
   },
+};
+
+createAutoComplete({
+  root: document.querySelector("#left-autocomplete"),
+  ...autoCompleteConfig,
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie , document.querySelector("#left-summary"));
+  },
 });
 
-const onMovieSelect = async (movie) => {
+createAutoComplete({
+  root: document.querySelector("#right-autocomplete"),
+  ...autoCompleteConfig,
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie , document.querySelector("#right-summary"));
+  },
+});
+
+const onMovieSelect = async (movie , summaryElement) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       i: movie.imdbID,
@@ -36,7 +50,7 @@ const onMovieSelect = async (movie) => {
     },
   });
 
-  document.querySelector("#summary").innerHTML = movieTeplate(response.data);
+  summaryElement.innerHTML = movieTeplate(response.data);
 };
 
 const movieTeplate = (movieDetail) => {
