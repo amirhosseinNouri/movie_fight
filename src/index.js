9b18095ee8d82ec1a96754a8ad917115a94a6a3c
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-01 11:43:36 +0330

    added auto complete and drop down menu

--- src/index.js
+++ src/index.js
@@ -6,12 +6,48 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) return [];
+
+  return response.data.Search;
 };
+
+const root = document.querySelector(".autocomplete");
+
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown")
+const resultsWrapper = document.querySelector(".dropdown-content")
 
-const onInput = (event) => {
-  fetchData(event.target.value);
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  resultsWrapper.innerHTML = ''
+  dropdown.classList.add("is-active")
+
+  for (movie of movies) {
+
+    const option = document.createElement("a");
+
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
+    option.classList.add("dropdown-item")
+
+    option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+    
+
+    resultsWrapper.appendChild(option);
+  }
 };
-input.addEventListener("input", debounce(onInput , 500));
+input.addEventListener("input", debounce(onInput, 500));
